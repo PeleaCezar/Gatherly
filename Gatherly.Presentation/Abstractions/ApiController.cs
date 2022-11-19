b9686565e7848b06e5e2810e0d21f6bf99e0e71b
Cezar <peleacezar@icloud.com>
2022-11-19 10:57:53 +0200

    Add fluent validation into project

--- Gatherly.Presentation/Abstractions/ApiController.cs
+++ Gatherly.Presentation/Abstractions/ApiController.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Gatherly.Domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gatherly.Presentation.Abstractions
@@ -11,5 +13,37 @@ namespace Gatherly.Presentation.Abstractions
         {
             Sender = sender;
         }
+
+        protected IActionResult HandleFailure(Result result) =>
+            result switch
+            {
+                { IsSuccess: true } => throw new InvalidOperationException(),
+                IValidationResult validationResult =>
+                BadRequest(
+                    CreateProblemDetails(
+                        "Validation Error",
+                        StatusCodes.Status400BadRequest,
+                        result.Error,
+                        validationResult.Errors)),
+                _ => BadRequest(
+                    CreateProblemDetails("Bad Request",
+                        StatusCodes.Status400BadRequest,
+                        result.Error))
+            };
+
+        private static ProblemDetails CreateProblemDetails(
+            string title,
+            int status,
+            Error error,
+            Error[]? errors = null) =>
+            new()
+            {
+                Title = title,
+                Type = error.Code,
+                Detail = error.Message,
+                Status = status,
+                Extensions = { { nameof(errors), errors } }
+            };
+
     }
 }

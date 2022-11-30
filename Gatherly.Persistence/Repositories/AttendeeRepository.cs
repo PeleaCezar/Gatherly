e66d62824d2ecae96b786f3f1e2972b5043a5820
Cezar <peleacezar@icloud.com>
2022-11-30 10:23:34 +0200

    Add AttendeeRepository

---
+++ Gatherly.Persistence/Repositories/AttendeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Domain.Repositories;
+
+namespace Gatherly.Persistence.Repositories
+{
+    internal sealed class AttendeeRepository : IAttendeeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AttendeeRepository(ApplicationDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public void Add(Attendee attendee) =>
+            _dbContext.Set<Attendee>().Add(attendee);
+    }
+}

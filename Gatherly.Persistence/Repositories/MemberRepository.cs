db3dda4afd9fe403f553c029234a6c4e826fdbba
Cezar <peleacezar@icloud.com>
2022-12-26 19:05:57 +0200

    Add dapper in GetMemberByIdQueryHandler

--- Gatherly.Persistence/Repositories/MemberRepository.cs
+++ Gatherly.Persistence/Repositories/MemberRepository.cs
@@ -1,23 +1,47 @@
-﻿using Gatherly.Domain.Entities;
+﻿using Dapper;
+using Gatherly.Application.Abstractions;
+using Gatherly.Domain.Entities;
 using Gatherly.Domain.Repositories;
 using Gatherly.Domain.ValueObjects;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gatherly.Persistence.Repositories
 {
-
     public sealed class MemberRepository : IMemberRepository
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly ISqlConnectionFactory _connectionFactory;
 
-        public MemberRepository(ApplicationDbContext dbContext) =>
+        public MemberRepository(ApplicationDbContext dbContext, ISqlConnectionFactory connectionFactory)
+        {
             _dbContext = dbContext;
+            _connectionFactory = connectionFactory;
+        }
+            
 
         public async Task<Member> GetByIdAsync(Guid id, CancellationToken cancellationToken = default) =>
             await _dbContext
                 .Set<Member>()
                 .FirstOrDefaultAsync(member => member.Id == id, cancellationToken);
 
+        public async Task<Member> GetByIdWithDapperAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            await using SqlConnection sqlConnection = _connectionFactory.CreateConnection();
+
+            Member member = await sqlConnection
+                 .QueryFirstOrDefaultAsync<Member>(
+                 @"SELECT Id, Email, FirstName, LastName 
+                   FROM Members
+                   WHERE Id = @MemberId",
+                  new
+                  {
+                     id
+                  });
+
+            return member;
+        }
+            
         public async Task<Member> GetByEmailAsync(Email email, CancellationToken cancellationToken = default) =>
              await _dbContext
                 .Set<Member>()
